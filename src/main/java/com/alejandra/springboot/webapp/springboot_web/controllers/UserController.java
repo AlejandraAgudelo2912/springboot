1c5957ee008055d87c08dfe29e2dd51373c44b4a
LAPTOP-1DQNJ8L3\aleja <339210@alu.murciaeduca.es>
2026-03-25 18:28:47 +0100

    clase dos: pasar datos a la vista

--- src/main/java/com/alejandra/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/alejandra/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.alejandra.springboot.webapp.springboot_web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -8,7 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class UserController {
 
     @GetMapping("/details") //el mas tipico para indicar la ruta 
-    public String details(){
+    public String details(Model model){ //aqui solo podemos pasar strings 
+        model.addAttribute("title", "Hola mundo spring boot"); //param que pasamos a la vista con el model.add... añadimos cosas que queremos pasarle a la vista
+        model.addAttribute("name", "alejandra");
+        model.addAttribute("lastname", "agudelo");
 
         return "details"; //devolvemos las plantillas
     }
