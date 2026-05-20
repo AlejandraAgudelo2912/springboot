9a6edc8e17db2c274d3d285de3e1480d11b5a2fd
LAPTOP-1DQNJ8L3\aleja <339210@alu.murciaeduca.es>
2026-05-20 19:16:59 +0200

    Clase 4: DTO

--- src/main/java/com/alejandra/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/alejandra/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -1,11 +1,12 @@
 package com.alejandra.springboot.webapp.springboot_web.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
+//import java.util.HashMap;
+//import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.alejandra.springboot.webapp.springboot_web.models.User;
+import com.alejandra.springboot.webapp.springboot_web.models.dto.UserDto;
 
 
 @RestController
@@ -13,13 +14,19 @@ import com.alejandra.springboot.webapp.springboot_web.models.User;
 public class UserRestController {
 
     @GetMapping("/details") //el mas tipico para indicar la ruta 
-    public Map<String, Object> details(){ //aqui solo podemos pasar strings 
+    public UserDto details(){ //aqui solo podemos pasar strings 
+
+        UserDto userDto = new UserDto();
         User user = new User("alejandra", "agudelo");
-        Map<String, Object> body = new HashMap<>();
+        userDto.setUser(user);
+        userDto.setTittle("Hola mundo spring boot");
 
-        body.put("title", "Hola mundo spring boot"); //param que pasamos a la vista con el model.add... añadimos cosas que queremos pasarle a la vista
-        body.put("user", user );
+        //Map<String, Object> body = new HashMap<>();
 
-        return body; //devolvemos el json
+        //body.put("title", "Hola mundo spring boot"); //param que pasamos a la vista con el model.add... añadimos cosas que queremos pasarle a la vista
+        //body.put("user", user );
+
+
+        return userDto; //devolvemos el json
     }
 }
