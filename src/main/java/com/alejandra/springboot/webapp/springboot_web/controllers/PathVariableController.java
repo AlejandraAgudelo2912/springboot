4c8a6a1e6f72ceb2488af2330990226d0979ee31
LAPTOP-1DQNJ8L3\aleja <339210@alu.murciaeduca.es>
2026-08-20 13:56:58 +0200

    Api Rest enviar Json petición POST

--- src/main/java/com/alejandra/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/alejandra/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -3,6 +3,7 @@ package com.alejandra.springboot.webapp.springboot_web.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alejandra.springboot.webapp.springboot_web.models.User;
 import com.alejandra.springboot.webapp.springboot_web.models.dto.ParamDto;
 
 import java.util.HashMap;
@@ -11,6 +12,9 @@ import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
@@ -32,6 +36,14 @@ public class PathVariableController {
         json.put("id", id);
 
         return json;
+    }
+    
+    @PostMapping("/create")
+    public User create(@RequestBody User user) {
+        //hacer algo con el ususario por ejmeplo save en la bbdd
+        user.setName(user.getName().toUpperCase());
+        
+        return user;
     }
     
     
