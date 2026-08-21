ccd7a6122fccc81b5818577a7522a28a77a16c15
LAPTOP-1DQNJ8L3\aleja <339210@alu.murciaeduca.es>
2026-08-21 19:51:22 +0200

    Agregando otros archivos properties personalizados para las configuraciones

--- src/main/java/com/alejandra/springboot/webapp/springboot_web/SpringbootWebApplication.java
+++ src/main/java/com/alejandra/springboot/webapp/springboot_web/SpringbootWebApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// @PropertySources({
+// 	@PropertySource("classpath:values.properties")
+// }) Podemos tener varios archivos de propiedades y leerlos en la aplicacion
+//@PropertySource("classpath:values.properties") //para poder leer el archivo values.properties y poder inyectar los valores en las variables de la clase PathVariableController
 public class SpringbootWebApplication {
 
 	public static void main(String[] args) {
