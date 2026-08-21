9e01d491112e812a27e753b85e4b5a4e69afa19e
LAPTOP-1DQNJ8L3\aleja <339210@alu.murciaeduca.es>
2026-08-21 19:57:56 +0200

    Encoding charset en properties

--- src/main/java/com/alejandra/springboot/webapp/springboot_web/ValuesConfig.java
+++ src/main/java/com/alejandra/springboot/webapp/springboot_web/ValuesConfig.java
@@ -6,8 +6,11 @@ import org.springframework.context.annotation.PropertySources;
 
 @Configuration
 @PropertySources({
- 	@PropertySource("classpath:values.properties")
+ 	@PropertySource(value="classpath:values.properties", encoding="UTF-8")
 }) 
+//otra alternativa es guardar el archivo values.properties con el encoding ISO-8859-1 en visual studio code
+
+//@PropertySource(value="classpath:values.properties", encoding="UTF-8") para leer el archivo y detectar los caracteres especiales como acentos y �, si no se pone el encoding se mostrar� mal en la consola y en el navegador
 public class ValuesConfig {
 
 }
