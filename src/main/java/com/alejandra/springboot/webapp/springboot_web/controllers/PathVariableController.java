f5becfd6d8426aef9792c10b4d63460932ec69ef
LAPTOP-1DQNJ8L3\aleja <339210@alu.murciaeduca.es>
2026-08-22 18:35:04 +0200

    Objeto Environment de Spring para leer configuraciones

--- src/main/java/com/alejandra/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/alejandra/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -10,7 +10,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -53,6 +55,10 @@ public class PathVariableController {
     @Value("#{${config.valuesMap}.price}")
     private Long price;
 
+    @Autowired
+    private Environment environment;
+    //el environment nos permite acceder a las propiedades de configuracion de la aplicacion
+
     @GetMapping("/baz/{message}")
     public ParamDto baz(@PathVariable() String message) {
 
@@ -81,13 +87,22 @@ public class PathVariableController {
 
     @GetMapping("/values")
     public Map<String, Object> values(@Value("${config.message}") String message ){
+        Long code2 = environment.getProperty("config.code", Long.class);
+        //lo podemos convertir directamente en vez de hacerlo en el json.put al final es lo mismo da igual
         Map<String, Object> json = new HashMap<>();
         json.put("username", username);
+
         json.put("message", message);
+        json.put("message2", environment.getProperty("config.message"));
+
         json.put("listOfValues", listOfValues);
         json.put("valueString", valueString);
         json.put("valueList", valueList);
+
         json.put("code", code);
+        json.put("code2", code2);
+        //json.put("code2", environment.getProperty("config.code", Long.class));
+
         json.put("valuesMap", valuesMap);
         json.put("product", product);
         json.put("price", price);
